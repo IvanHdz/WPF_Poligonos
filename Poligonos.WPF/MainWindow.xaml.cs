ed9f04babd182ffc7cf5cef01707210be1141c60
Jesús Iván Hernández Ramírez <Jesus_Hdez.Ramirez@outlook.com>
2020-09-23 13:43:34 -0500

    Commit

---
+++ Poligonos.WPF/MainWindow.xaml.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Windows;
+
+namespace Poligonos.WPF
+{
+    /// <summary>
+    /// Lógica de interacción para MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+
+            for (int i = 0; i < 71; i++)
+            {
+                double x = i * Math.PI;
+                double y = 40 + 30 * Math.Sin(x / 10);
+                polygon1.Points.Add(new Point(x, y));
+            }
+        }
+    }
+}
